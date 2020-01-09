f7f34358ae52e0924972b8977fc9c6ceff4a7326
pkesha <parham.keshavarzi7117@gmail.com>
2020-01-09 10:32:48 -0600

    Update to LinkedList Challenge. Bugs in Album class, line 15

--- Section_8/LinkedListChallenge/src/com/company/Main.java
+++ Section_8/LinkedListChallenge/src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
+    private static ArrayList<Album> albums = new ArrayList<Album>();
 
     public static void main(String[] args) {
-	// write your code here
+        Album album = new Album("Album 1");
+        album.addSongAlbum("Song 1", 120);
+        album.addSongAlbum("4311 2", 150);
+        album.addSongAlbum("Song 3", 180);
+        albums.add(album);
+
     }
 }
