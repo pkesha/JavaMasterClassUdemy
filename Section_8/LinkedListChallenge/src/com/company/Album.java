df91fdbedd17808521c9f048e31e98ae6558ede2
pkesha <parham.keshavarzi7117@gmail.com>
2020-01-09 10:45:38 -0600

    Update to LinkedList Challenge. Bugs in Album class, line 15

---
+++ Section_8/LinkedListChallenge/src/com/company/Album.java
@@ -0,0 +1,81 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+    private String albumName;
+    private ArrayList<Song> songsAlbums;
+
+    public Album(String albumName) {
+        this.albumName = albumName;
+        this.songsAlbums = new ArrayList<Song>();
+    }
+
+    private Song findSongAlbum(String title){
+        for (Song checkedSong: this.songsAlbums){
+            //System.out.println(checkedSong.getSongName());
+            System.out.println("Boolean: " + (checkedSong.getSongName() == title));
+
+            //Shows as false but returns the 1st value for checked song after the first element is added
+            if(checkedSong.getSongName() == title);{
+                //System.out.println("Song checked: " + checkedSong.getSongName());
+                //System.out.println("Input song: " + title);
+                return checkedSong;
+            }
+        }
+        return null;
+    }
+
+    public boolean addSongAlbum(String songName, int timeSeconds){
+        Song newSong = new Song(songName, timeSeconds);
+        if(findSongAlbum(songName) == null){
+            this.songsAlbums.add(newSong);
+            System.out.println("Song " + songName + " was added to album " +
+                    this.albumName);
+            return true;
+        } else {
+            System.out.println("BOOLEAN: " + findSongAlbum(songName).getSongName());
+            System.out.println("Song " + songName + " already exists");
+            return false;
+        }
+    }
+
+    //Using track number (index from Album array list) to add to playlist.
+    public boolean addSongPlaylist(int trackNumber, LinkedList<Song> songsPlaylist){
+        int index = trackNumber - 1;
+        if ((this.songsAlbums.size() >= index) &&
+                (index >= 0)){
+            //Adding to linkedlist.  No, the index is not relevant for linkedlist
+            //Just for choosing which song to add in the albums arraylist
+            songsPlaylist.add(this.songsAlbums.get(index));
+            return true;
+        }
+        System.out.println("This album exists but does not contain any songs");
+        return false;
+    }
+
+    public boolean addSongPlaylist(String name, LinkedList<Song> songsPlaylist){
+        Song addSong = findSongAlbum(name);
+        if(addSong != null){
+            songsPlaylist.add(addSong);
+            return true;
+        } else{
+            System.out.println("Did not find song " + name);
+            return false;
+        }
+    }
+
+    public boolean removeSongAlbum(String songName){
+        Song removeSong = findSongAlbum(songName);
+
+        if (removeSong != null){
+            this.songsAlbums.remove(removeSong);
+            System.out.println("Song " + songName + " was removed");
+            return true;
+        }
+        System.out.println("The song " + songName + " was not found");
+        return false;
+    }
+}
+
