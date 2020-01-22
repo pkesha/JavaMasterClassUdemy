2a329ede41b930e8bbfce299b157ce6711c03530
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2020-01-22 16:08:17 -0600

    Update ISaveable.java

--- Section_9/InterfaceChallenge/src/com/company/ISaveable.java
+++ Section_9/InterfaceChallenge/src/com/company/ISaveable.java
@@ -3,6 +3,7 @@ package com.company;
 import java.util.ArrayList;
 
 public interface ISaveable {
+    //Update
     ArrayList<String> read = new ArrayList<String>();
     ArrayList<String> write = new ArrayList<String>();
 
