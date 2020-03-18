3f8ed128c101f6ec9701408a4a55b70cec6fcfaa
pkesha <parham.keshavarzi7117@gmail.com>
2020-03-18 12:41:01 -0500

    "Abstract Class Challenge"

--- Section_9/AbstractClassesChallenge/src/com/company/Main.java
+++ Section_9/AbstractClassesChallenge/src/com/company/Main.java
@@ -5,20 +5,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        MyLinkedList list = new MyLinkedList(null);
-
+        //MyLinkedList list = new MyLinkedList(null);
+        SearchTree tree = new SearchTree(null);
         String stringData = "5 9 0 2 8 5 0 1";
 
         String[] data = stringData.split(" ");
         for (String s : data){
-            list.addItem(new Node(s));
+            tree.addItem(new Node(s));
         }
-        list.traverse(list.getRoot());
+
+        tree.traverse(tree.getRoot());
+        /*list.traverse(list.getRoot());
 
         list.removeItem(new Node("4"));
         list.removeItem(new Node ("5"));
 
         System.out.println();
-        list.traverse(list.getRoot());
+        list.traverse(list.getRoot());*/
+
     }
 }
