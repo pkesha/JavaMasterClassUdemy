a8131234051659c07e6adead4865b51a7240f732
pkesha <parham.keshavarzi7117@gmail.com>
2020-03-17 23:21:47 -0500

    "Abstract Class Challenge"

--- Section_9/AbstractClassesChallenge/src/com/company/Main.java
+++ Section_9/AbstractClassesChallenge/src/com/company/Main.java
@@ -1,9 +1,24 @@
 package com.company;
 
+//
+
 public class Main {
 
     public static void main(String[] args) {
+        MyLinkedList list = new MyLinkedList(null);
 
+        String stringData = "5 8 9 0 2 2 3";
 
+        String[] data = stringData.split(" ");
+        for (String s : data){
+            list.addItem(new Node(s));
+        }
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("4"));
+        list.removeItem(new Node ("5"));
+
+        System.out.println();
+        list.traverse(list.getRoot());
     }
 }
