a07d0995949d3d2cbb25442640ffb3774fad5e24
pkesha <parham.keshavarzi7117@gmail.com>
2020-04-09 22:24:00 -0500

    "Input and Output resource files"

--- Section_14/InputOutput/src/com/timbuchalka/Locations.java
+++ Section_14/InputOutput/src/com/timbuchalka/Locations.java
@@ -1,5 +1,7 @@
 package com.timbuchalka;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,16 +13,47 @@ import java.util.Set;
 public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
 
+    public static void main(String[] args) {
+        FileWriter locFile = null;
+        try {
+            //File Writer object
+            locFile = new FileWriter("locations.txt");
+            for(Location location : locations.values()){
+                locFile.write(location.getLocationID() + ", " + location.getDescription()
+                        + "\n");
+            }
+            //If there is an error within the for loop, it will leave the file open
+
+        } catch (IOException e){
+            System.out.println("In catch block");
+            e.printStackTrace();
+        } finally {
+            System.out.println("In finally block");
+            try {
+                if (locFile != null) {
+                    System.out.println("Attempting to close locFile");
+                    locFile.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    //This is so everything is created once
     static {
         Map<String, Integer> tempExit = new HashMap<String, Integer>();
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",null));
+        locations.put(0, new Location(0,
+                "You are sitting in front of a computer learning Java",null));
 
         tempExit = new HashMap<String, Integer>();
         tempExit.put("W", 2);
         tempExit.put("E", 3);
         tempExit.put("S", 4);
         tempExit.put("N", 5);
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building",tempExit));
+        locations.put(1, new Location(1,
+                "You are standing at the end of a road before a small brick building",tempExit));
 
         tempExit = new HashMap<String, Integer>();
         tempExit.put("N", 5);
@@ -28,12 +61,14 @@ public class Locations implements Map<Integer, Location> {
 
         tempExit = new HashMap<String, Integer>();
         tempExit.put("W", 1);
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring",tempExit));
+        locations.put(3, new Location(3,
+                "You are inside a building, a well house for a small spring",tempExit));
 
         tempExit = new HashMap<String, Integer>();
         tempExit.put("N", 1);
         tempExit.put("W", 2);
-        locations.put(4, new Location(4, "You are in a valley beside a stream",tempExit));
+        locations.put(4, new Location(4,
+                "You are in a valley beside a stream",tempExit));
 
         tempExit = new HashMap<String, Integer>();
         tempExit.put("S", 1);
--- Section_14/InputOutput/src/com/timbuchalka/Main.java
+++ Section_14/InputOutput/src/com/timbuchalka/Main.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-
 public class Main {
     private static Locations locations = new Locations();
 
