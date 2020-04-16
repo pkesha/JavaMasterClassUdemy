17fbc2d1878859a25882973729e635f04a895e11
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2020-04-16 13:58:28 -0500

    Random File Access

--- Section_14/AdventureRandom/src/com/timbuchalka/Main.java
+++ Section_14/AdventureRandom/src/com/timbuchalka/Main.java
@@ -1,6 +1,5 @@
 package com.timbuchalka;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -8,8 +7,7 @@ import java.util.Scanner;
 public class Main {
     private static Locations locations = new Locations();
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) {
 
 	    Scanner scanner = new Scanner(System.in);
 
@@ -20,16 +18,16 @@ public class Main {
         vocabulary.put("WEST", "W");
         vocabulary.put("EAST", "E");
 
-        Location currentLocation = locations.getLocation(64);
+        int loc = 64;
+//        int loc = 1;
         while(true) {
-            System.out.println(locations.get(currentLocation).getDescription());
+            System.out.println(locations.get(loc).getDescription());
 
-            //fdas
-            if(currentLocation.getLocationID() == 0) {
+            if(loc == 0) {
                 break;
             }
 
-            Map<String, Integer> exits = currentLocation.getExits();
+            Map<String, Integer> exits = locations.get(loc).getExits();
             System.out.print("Available exits are ");
             for(String exit: exits.keySet()) {
                 System.out.print(exit + ", ");
@@ -48,13 +46,12 @@ public class Main {
             }
 
             if(exits.containsKey(direction)) {
-                currentLocation = locations.getLocation(currentLocation.getExits().get(direction));
+                loc = exits.get(direction);
 
             } else {
                 System.out.println("You cannot go in that direction");
             }
         }
 
-        locations.close();
     }
 }
