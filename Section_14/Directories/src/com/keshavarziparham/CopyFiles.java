aa03865e8f200e9d136bf0052513e800df8e4d96
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2020-04-23 11:58:59 -0500

    "Copy Entire Tree"

---
+++ Section_14/Directories/src/com/keshavarziparham/CopyFiles.java
@@ -0,0 +1,22 @@
+package com.keshavarziparham;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class CopyFiles extends SimpleFileVisitor<Path> {
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        System.out.println("Error accessing file " + file. toAbsolutePath() + " " + exc.getMessage());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        String help;
+
+    }
+}
