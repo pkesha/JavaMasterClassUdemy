25ae36c2f78ce21672e5af31fb59195a1f2b46b6
pkesha <parham.keshavarzi7117@gmail.com>
2020-04-27 18:28:21 -0500

    "Multiple Threads"

--- Section_15/Counter/src/com/keshavarziparham/Main.java
+++ Section_15/Counter/src/com/keshavarziparham/Main.java
@@ -3,24 +3,39 @@ package com.keshavarziparham;
 public class Main {
 
     public static void main(String[] args) {
-        Countdown countdown = new Countdown();
+        //From lecture 268 - Thread variables
+//        Countdown countdown = new Countdown();
+//
+//        CountdownThread t1 = new CountdownThread(countdown);
+//        t1.setName("Thread 1");
+//        CountdownThread t2 = new CountdownThread(countdown);
+//        t2.setName("Thread 2");
+//
+//        t1.start();
+//        t2.start();
 
-        CountdownThread t1 = new CountdownThread(countdown);
+        //Lecture 269 - Synchronization
+
+        Countdown countdown1 = new Countdown();
+        Countdown countdown2 = new Countdown();
+
+        CountdownThread t1 = new CountdownThread(countdown1);
         t1.setName("Thread 1");
-        CountdownThread t2 = new CountdownThread(countdown);
+        CountdownThread t2 = new CountdownThread(countdown2);
         t2.setName("Thread 2");
 
         t1.start();
         t2.start();
+
+
     }
-
-
 }
 
 class Countdown{
+    //All threads will now share the same value of i, have to declare it in for loop
     private int i;
 
-    public void doCountdown(){
+    public synchronized void doCountdown(){
         String color;
 
         switch (Thread.currentThread().getName()){
@@ -35,6 +50,7 @@ class Countdown{
                 break;
         }
 
+        //If i was declared initialized in the for-loop, the value would not be shared by threads (why?)
         for(i = 10; i > 0; i--){
             System.out.println(color + Thread.currentThread().getName() + ": i = " + i);
         }
