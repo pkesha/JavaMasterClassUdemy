e644b852295f661df551a4d761e51b9f3989a9a5
pkesha <parham.keshavarzi7117@gmail.com>
2020-05-03 06:38:38 -0500

    Using try with finally Threads - lock

--- Section_15/ProducerConsumer/src/com/keshavarziparham/Main.java
+++ Section_15/ProducerConsumer/src/com/keshavarziparham/Main.java
@@ -3,6 +3,7 @@ package com.keshavarziparham;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 import static com.keshavarziparham.Main.EOF;
@@ -16,13 +17,30 @@ public class Main {
 
         ReentrantLock bufferlock = new ReentrantLock();
 
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
         MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_YELLOW, bufferlock);
         MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE, bufferlock);
         MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN, bufferlock);
 
+        executorService.execute(producer);
+        executorService.execute(consumer1);
+        executorService.execute(consumer2);
+
+        Future<String> future = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println(ThreadColor.ANSI_WHITE + "I'm being printed for the Callable class");
+                return "This is the callable result";
+            }
+        });
+
+        executorService.shutdown();
+        /*
         new Thread(producer).start();
         new Thread(consumer1).start();
         new Thread(consumer2).start();
+         */
     }
 }
 
