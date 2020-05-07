cf2ca2bff4082cd64a63a1359d90af9ced3ff752
owner <parham.keshavarzi7117@gmail.com>
2020-05-07 12:11:25 -0500

    "Fair locks"

--- Section_15/Starvation/src/com/parhamkeshavarzi/Main.java
+++ Section_15/Starvation/src/com/parhamkeshavarzi/Main.java
@@ -1,8 +1,12 @@
 package com.parhamkeshavarzi;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
     //All threads share the same object since its static
-    private static Object lock = new Object();
+    //private static Object lock = new Object();
+    //Fair lock - no starvation problems
+    private static ReentrantLock lock = new ReentrantLock(true);
 
     public static void main(String[] args) {
         Thread t1 = new Thread(new Worker(ThreadColor.ANSI_RED), "Priority 10");
@@ -11,11 +15,11 @@ public class Main {
         Thread t4 = new Thread(new Worker(ThreadColor.ANSI_CYAN), "Priority 4");
         Thread t5 = new Thread(new Worker(ThreadColor.ANSI_PURPLE), "Priority 2");
 
-//        t1.setPriority(10);
-//        t2.setPriority(8);
-//        t3.setPriority(6);
-//        t4.setPriority(4);
-//        t5.setPriority(2);
+        t1.setPriority(10);
+        t2.setPriority(8);
+        t3.setPriority(6);
+        t4.setPriority(4);
+        t5.setPriority(2);
 
         t3.start();
         t2.start();
@@ -38,10 +42,13 @@ public class Main {
         @Override
         public void run() {
             for(int i = 0; i < 100; i++){
-                synchronized (lock){
+                lock.lock();
+                try {
                     System.out.format(threadColor + "%s: runCount = %d\n",
                             Thread.currentThread().getName(), runCount++);
                     //execute critical section of code
+                }finally {
+                    lock.unlock();
                 }
             }
         }
