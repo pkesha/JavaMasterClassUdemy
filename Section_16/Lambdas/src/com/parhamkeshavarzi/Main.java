e04fe31eb8c239a01938e6e2dd6ed3b928172e00
owner <parham.keshavarzi7117@gmail.com>
2020-05-14 18:02:27 -0500

    "Lambda Expressions intro"

---
+++ Section_16/Lambdas/src/com/parhamkeshavarzi/Main.java
@@ -0,0 +1,21 @@
+package com.parhamkeshavarzi;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Printing from the Runnable");
+            }
+        }).start();
+    }
+}
+
+
+class CodeToRun implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Printing from  Runnable");
+    }
+}
