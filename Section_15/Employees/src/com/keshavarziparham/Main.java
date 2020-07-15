4dc658ceb4fda7b2fde3f980966240bc338b2cfd
pkesha <parham.keshavarzi7117@gmail.com>
2020-07-15 15:34:50 -0500

    "More predicates and suppliers"

--- Section_15/Employees/src/com/keshavarziparham/Main.java
+++ Section_15/Employees/src/com/keshavarziparham/Main.java
@@ -3,12 +3,13 @@ package com.keshavarziparham;
 import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
+import java.util.Random;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
-
         Employee john = new Employee("John Doe", 33);
         Employee parham = new Employee("Parham Keshavarzi", 21);
         Employee jack = new Employee("Jack Hill", 22);
@@ -29,24 +30,35 @@ public class Main {
         printEmployeesByAge(employees, "\nEmployees 30 and under",
                 employee ->  employee.getAge() <= 30);
 
-        /*
-        predicate arguements are lambda expressions that match the predicate interface
-         */
-        printEmployeesByAge(employees,"Employeees over 30", employee -> employee.getAge() > 30);
-        printEmployeesByAge(employees, "Employess under 30", employee -> employee.getAge() <= 30);
-        //Anonmyous class
-        printEmployeesByAge(employees, "\nEmployees younger than 25", new Predicate<Employee>() {
+
+        //predicate arguments are lambda expressions that match the predicate interface
+        printEmployeesByAge(employees,"Employees over 30", employee -> employee.getAge() > 30);
+        printEmployeesByAge(employees, "Employees under 30", employee -> employee.getAge() <= 30);
+        //Anonymous class
+        printEmployeesByAge(employees, "\nEmployees younger than 25",
+                new Predicate<Employee>() {
             @Override
             public boolean test(Employee employee) {
                 return employee.getAge() < 25;
             }
         });
 
-        IntPredicate intp = i -> i > 15;
-        System.out.println(intp.test(10));
+        //Left of arrow is argument, right side is statement (the statements would then override)
+        IntPredicate greaterThan15 = i -> i > 15;
+        IntPredicate lessThan100 = i -> i < 100;
+        System.out.println(greaterThan15.test(10));
         int a = 20;
-        System.out.println(intp.test(a+5));
+        System.out.println(greaterThan15.test(a+5));
+        //True if all predicates return true.  Each variable is within the scope of a single lambda.
+        //This is why it's okay to use the same name
+        System.out.println(greaterThan15.and(lessThan100).test(50));
+        System.out.println(greaterThan15.and(lessThan100).test(15));
 
+        Random random = new Random();
+        Supplier<Integer> randomSupplier = () -> random.nextInt();
+        for(int i = 0; i < 10; i++){
+            System.out.println(random.nextInt(1000));
+        }
         /* L296 - deleted
         System.out.println("Employees over 30;");
         System.out.println("==================");
@@ -58,11 +70,10 @@ public class Main {
         test method is a functional method in this case and it accepts a predicate that returns a bool.
         Can replace both conditions (above and below) with predicate
          */
-        /*
-        Pass the requirement for each method a lambda expression that
-        meets the requirement of a consumer interface
-         */
         /* L296
+
+        //Pass the requirement for each method a lambda expression that
+        //meets the requirement of a consumer interface
 
         employees.forEach(employee -> {
             if(employee.getAge() > 30) {
@@ -85,7 +96,6 @@ public class Main {
             System.out.println(employee.getAge());
         });
         */
-
     }
 
 
