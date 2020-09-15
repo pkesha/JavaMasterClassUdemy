eeed248d21585eb35a97a660e90ba1994cf4e713
pkesha <parham.keshavarzi7117@gmail.com>
2020-09-15 14:22:44 -0500

    "Matcher find and Group Methods, And, Or, & Not"

--- Section_17/RegularExpressions/src/com/keshavarziparham/Main.java
+++ Section_17/RegularExpressions/src/com/keshavarziparham/Main.java
@@ -1,5 +1,8 @@
 package com.keshavarziparham;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -56,5 +59,79 @@ public class Main {
         //Useful to replace and add tags around a word
         System.out.println(hasWhitespace.replaceAll("\\b", "X"));
 
+        String thirdAlphaNumericString = "abcDeee12Ghiiiijkl99z";
+        //The 3 in curly braces indicates 3 letters of e - quantifier
+        System.out.println(alphanumeric.replaceAll("^abcDe{3}", "YYY"));
+        //Only replaces any characters "abcDe" - do not care about the rest
+        System.out.println(thirdAlphaNumericString.replaceAll("^abcDe", "YYY"));
+        System.out.println(thirdAlphaNumericString.replaceAll("^abcDe*", "YYY"));
+        //Will match and String where the first chars have 2 - 5 e's after it
+        System.out.println(thirdAlphaNumericString.replaceAll("^abcDe{2,5}", "YYY"));
+        System.out.println(thirdAlphaNumericString.replaceAll("h+i*j", "Y"));
+
+        StringBuilder htmlText = new StringBuilder("<h1>My Heading<h1>");
+        htmlText.append("<p>This is a paragraph about something.</p>");
+        htmlText.append("<p>This is another paragraph about something else.</p>");
+        htmlText.append("<h2>Summary</h2>");
+        htmlText.append("<p>Here is the summary.</p>");
+
+        //Regular expression assigned to String
+        //Dot will match every character and star means zero or more
+        // - anything before or after will be a match
+        String h2Pattern = "<h2>";
+        //Pattern.CASE_INSENSITIVE - don't care about case
+        Pattern pattern = Pattern.compile(h2Pattern, Pattern.CASE_INSENSITIVE);
+        //Text want to check against
+        Matcher matcher = pattern.matcher(htmlText);
+        //Will match the String as a whole
+        //Internal state of matcher will be updated and needs to be reset
+        System.out.println(matcher.matches());
+
+        matcher.reset();
+        int count = 0;
+        while (matcher.find()){
+            count++;
+            System.out.println("Occurrence " + count + " : " + matcher.start() + " to "
+                    + matcher.end());
+        }
+
+        //The parenthesis is used to group regular expressions.  Not apart of the pattern
+        //The group represents h2 tags and anything between
+        String h2GroupPattern = "(<h2>.*?</h2>)";
+        Pattern groupPattern = Pattern.compile(h2GroupPattern);
+        Matcher groupMatcher = groupPattern.matcher(htmlText);
+        System.out.println(groupMatcher.matches());
+        groupMatcher.reset();
+
+        while(groupMatcher.find()){
+            System.out.println("Occurrences: " + groupMatcher.group(1));
+        }
+
+        //2 groups - <h2> and </h2>
+        String h2TextGroups = "(<h2>)(.+?)(</h2>)";
+        Pattern h2TextPattern = Pattern.compile(h2TextGroups);
+        Matcher h2TextMatcher = h2TextPattern.matcher(htmlText);
+
+        while(h2TextMatcher.find()){
+            System.out.println("Occurrence: " + h2TextMatcher.group(2));
+        }
+
+        //If capital or lower case H, consider it a match
+        System.out.println("harry".replaceAll("[H|h]arry", "Larry"));
+
+        //[^abc] - not character when carrot is first character
+        String tvTest = "tsTvtkt";
+        //T not followed by v
+        String tNotVRegExp = "t(?!v)";
+        Pattern tNotVPattern = Pattern.compile(tNotVRegExp);
+        Matcher tNotMatcher = tNotVPattern.matcher(tvTest);
+
+        count = 0;
+        while(tNotMatcher.find()){
+            count++;
+            System.out.println("Occurrences: " + count + " : " + tNotMatcher.start() + " to " +
+                    tNotMatcher.end());
+        }
+        //t{?=v} t followed by v but didn't want to include v in the match
     }
 }
