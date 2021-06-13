4caf7d7c7aadf933fc0f4137b779f2407a68269a
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-06-12 20:35:42 -0500

    L341

--- Section_19/Music/src/com/parhamkeshavarzi/Main.java
+++ Section_19/Music/src/com/parhamkeshavarzi/Main.java
@@ -1,8 +1,16 @@
 package com.parhamkeshavarzi;
 
+import model.DataSource;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        DataSource dataSource = new DataSource();
+        if(!dataSource.open()) {
+            System.out.println("Can't open database");
+            return;
+        }
+
+        dataSource.close();
     }
 }
