c5c41cb03c3148e4e561b80ce1c40b0c7026a704
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-06-21 16:57:29 -0500

    finish networking

--- Section_20/Networking/src/com/parhamkeshavarzi/Main.java
+++ Section_20/Networking/src/com/parhamkeshavarzi/Main.java
@@ -1,17 +1,45 @@
 package com.parhamkeshavarzi;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
+            URL url = new URL("https://api.flickr.com/services/feeds/photos_public.gne?tags=dogs");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Chrome");
+            connection.setReadTimeout(30000);
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response code: " + responseCode);
+
+            if(responseCode != 200) {
+                System.out.println("Error reading web page");
+                System.out.println(connection.getResponseMessage());
+                return;
+            }
+
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            String line = "";
+
+            while((line = inputReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            inputReader.close();
+
 //            URI uri = new URI("http://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
-            URI baseUri = new URI("http://username:password@myserver.com:5000");
-            URL url = baseUri.toURL();
+//            URI baseUri = new URI("http://username:password@myserver.com:5000");
+//            URL url = baseUri.toURL();
 
             //            System.out.println("Scheme = " + uri.getScheme());
 //            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
@@ -22,11 +50,32 @@ public class Main {
 //            System.out.println("Path = " + uri.getPath());
 //            System.out.println("Query = " + uri.getQuery());
 //            System.out.println("Fragment = " + uri.getFragment());
+//            URLConnection urlConnection = url.openConnection();
+//            urlConnection.setDoOutput(true);
+//            urlConnection.connect();
+//
+//            BufferedReader inputStream = new BufferedReader(
+//                    new InputStreamReader(urlConnection.getInputStream()));
+//
+//            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
+//            for(Map.Entry<String, List<String>> entry : headerFields.entrySet()) {
+//                String key = entry.getKey();
+//                List<String> value = entry.getValue();
+//                for(String string : value) {
+//                    System.out.println("value = " + value);
+//                }
+//            }
+//            String line = "";
+//            while(line != null) {
+//                line = inputStream.readLine();
+//                System.out.println(line);
+//            }
+//            inputStream.close();
 
-        } catch (URISyntaxException uriSyntaxException) {
-            System.out.println("URI Bad syntax: " + uriSyntaxException.getMessage());
         } catch (MalformedURLException malformedURLException) {
             System.out.println("URL Malformed: " + malformedURLException.getMessage());
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
         }
     }
 }
